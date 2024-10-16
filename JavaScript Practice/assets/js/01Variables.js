f01d9672f698b972b949f25e083cd4742b0e5c6b
antbojko <anthony.bojkovski@gmail.com>
2024-10-16 14:03:10 -0400

    Create 01Variables.js

---
+++ JavaScript Practice/assets/js/01Variables.js
@@ -0,0 +1,44 @@
+/* 
+  Developer: Anthony Bojkovski
+  Purpose: Performing variables in JavaScript
+*/
+
+// Declare a variable named schoolAddress and initialize it any George Brown College Campus address
+let schoolAddress = "200 King St E, Toronto, ON M5A 3W8, Canada";
+// Display the value of the variable schoolAddress on the browser console
+console.log(schoolAddress);
+// Update variable schoolAddress and assign another George Brown College Campus address
+schoolAddress = "160 Kendal Ave, Toronto, ON M5R 1M3, Canada";
+// Display the value of the variable schoolAddress on the browser console
+console.log(schoolAddress);
+// Declare variable that uses the camel case style and assign any value.
+let studentName = "John Doe";
+// Display the value of the variable on the browser console
+console.log(studentName);
+// Declare variable that uses the snake case style and assign any value.
+let student_name = "Jane Smith";
+// Display the value of the variable on the browser console
+console.log(student_name);
+// Declare a variable of your choice and assign any string of your choice
+let favoriteShow = "My favorite show is Dragon Ball Z."
+// Display the value of the variable on the browser console
+console.log(favoriteShow);
+// Declare a variable of your choice and assign any number of your choice
+let age = 23;
+// Display the value of the variable on the browser console
+console.log(age);
+// Declare a variable of your choice and assign any boolean of your choice
+let cloudyWeather = false;
+// Display the value of the variable on the browser console
+console.log(cloudyWeather);
+// Declare a variable of your choice and assign an array of either string or number of your choice
+let soccer = "My favorite sport is soccer";
+// Display the value of the variable on the browser console
+console.log(soccer);
+// Declare a variable named vehicle and assign the following properties and values:
+// make for example "Honda"
+// model for example "Civic"
+// vin for example "1GNEK13Z42R123456"
+let vehicle = "Honda, Civic, 1GNEK13Z42R123456";
+// Display the value of the variable on the browser console
+console.log(vehicle);
